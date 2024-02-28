d0a0b62d5b369dbd2d38dfa141dffbb30bbe3cd2
A-Behairi <behairiahmed@gmail.com>
2024-02-28 18:02:33 +0100

    fix: added iframe tests

--- cypress/components/IframeWrapper.cy.js
+++ cypress/components/IframeWrapper.cy.js
@@ -4,18 +4,45 @@ import IframeWrapper from '@/components/IframeWrapper.vue'
 
 describe('<IframeWrapper />', () => {
 
-  it('renders html', () => {
-    cy.fixture('../fixtures/hello-world.html').then(/** @param {string} html */(html) => {
-      const url = URL.createObjectURL(new Blob([html], { type: "text/html" })).toString()
-      cy.mount(IframeWrapper, {
-        props: {
-          src: url
-        }
-      }).then($vue => {
-        cy.get($vue.wrapper.wrapperElement)
-          .should('have.attr', 'src', url)
-        cy.get(/** @type {HTMLIFrameElement} */($vue.wrapper.wrapperElement).contentWindow?.document.getElementsByTagName("H2")).should('have.text', "Hello World")
+  describe("renders Hello World html", () => {
+
+    beforeEach(() => {
+      cy.fixture('../fixtures/hello-world.html').then(/** @param {string} html */(html) => {
+        const url = URL.createObjectURL(new Blob([html], { type: "text/html" })).toString()
+        cy.wrap(html).as('html')
+        cy.wrap(url).as('url')
+      })
+
+      cy.get('@url').then((url) => {
+        cy.vMount(IframeWrapper, {
+          props: {
+            src: url
+          }
+        })
       })
     })
+
+    it('iframe renders internal DOM', () => {
+      cy.get('@vue').then(vue => {
+        cy.get(/** @type {HTMLIFrameElement} */(vue.wrapper.element)).its('0.contentDocument').should('exist')
+      })
+    })
+
+    it('external html contains "Hello world"', () => {
+      cy.get('@html').then(html => {
+        expect(html).to.contain('Hello World')
+      })
+    })
+
+    it('iframe contains "Hello World"', () => {
+      cy.get('@vue').then(vue => {
+        cy.get(/** @type {HTMLIFrameElement} */(vue.wrapper.element)).
+          its('0.contentDocument')
+          .its('body')
+          .should('contain', "Hello World")
+      })
+    })
+
+
   })
 })
