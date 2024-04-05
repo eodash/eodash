5b77c3103a8f2d7595e737c78b319e9ed5c2d6f9
A-Behairi <behairiahmed@gmail.com>
2024-04-05 06:42:51 +0200

    chore: remove integration tests

--- cypress/integration/MobileLayout.cy.js
+++ cypress/integration/MobileLayout.cy.js
@@ -1,24 +1,25 @@
-import MobileLayout from '@/components/MobileLayout.vue'
-import { eodashConfigKey } from '@/store/Keys'
+// import MobileLayout from '@/components/MobileLayout.vue'
+// import { eodashKey } from '@/store/Keys'
 
-describe('<MobileLayout />', () => {
-  beforeEach(() => {
-    cy.vMount(MobileLayout)
-  })
+// describe('<MobileLayout />', () => {
+//   beforeEach(() => {
+//     cy.vMount(MobileLayout)
+//   })
 
-  it('renders successfully', () => {
-    cy.get("@vue").should('exist')
-    cy.get('.v-row').should('exist')
-  })
+//   it('renders successfully', () => {
+//     cy.get("@vue").should('exist')
+//     cy.get('.v-row').should('exist')
+//   })
 
-  it('renders statically configured titles in tabs ', () => {
-    //@ts-ignore
-    cy.get('@vue').then(({ options }) => {
-      /** @type {StaticWidget[]} */
-      (options.global.provide[eodashConfigKey].template.widgets).forEach((widget, idx) => {
-        cy.get(`.v-row > :nth-child(${idx + 1})`).contains(widget.title)
-      })
-    })
-  })
+//   it('renders titles in tabs ', () => {
+//     //@ts-ignore
+//     cy.get('@vue').then(({ options }) => {
+//       /** @type {import('@/types').Widget[]} */
+//       (options.global.provide[eodashKey].template.widgets).forEach((widget, idx) => {
+//         const title = "defineWidget" in widget ? widget.defineWidget(null).title : widget.title
+//         cy.get(`value[${idx}]`).contains(title)
+//       })
+//     })
+//   })
 
-})
+// })
