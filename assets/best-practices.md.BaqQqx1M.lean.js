00cfd11a3318959f32dab698b8c7d2a4b1e6d9a9
A-Behairi <behairiahmed@gmail.com>
2024-05-06 14:21:23 +0000

    Deploying to gh-pages from @ eodash/eodash@5f674ad7f8377965e420877f8d59d4f21e41ed56 🚀

--- assets/best-practices.md.BaqQqx1M.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a}from"./chunks/framework.Dl6Ui02Z.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"best-practices.md","filePath":"best-practices.md"}'),s={name:"best-practices.md"};function c(r,o,n,i,p,_){return a(),t("div")}const f=e(s,[["render",c]]);export{m as __pageData,f as default};
