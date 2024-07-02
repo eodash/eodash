8f6ae4896f919d56ff3e8ca088f276aea4d65990
A-Behairi <behairiahmed@gmail.com>
2024-07-02 12:12:09 +0200

    fix: cleanup export state

--- widgets/ExportState.vue
+++ widgets/ExportState.vue
@@ -1,31 +1,31 @@
 <template>
   <PopUp v-model="dialog">
     <v-card>
-      <v-card-title class="background-primary position-sticky">
-        <h4 class="text-headline text-white">
+      <v-card-title class="bg-primary">
+        <h5 class="text-h5">
           Storytelling map configuration
-        </h4>
+        </h5>
       </v-card-title>
 
       <v-card-text class="py-5">
         <p class="text-body-2">
           Copy and paste this code into the map layers field of the storytelling editor:
         </p>
-        <div class="pa-3 code-block text-body-2">
+        <div class="pa-3 code-block">
           {{ getLayers(props.for) }}
         </div>
 
         <div style="position: absolute; bottom: 15px;">
           <v-expand-transition>
-            <div v-if="copySuccess" class="success--text mr-3">
+            <div v-if="copySuccess" class="text-success mr-3">
               <v-icon color="success" left :icon="[mdiClipboardCheckOutline]" />
               <small>copied!</small>
             </div>
           </v-expand-transition>
         </div>
-        <v-row class="d-flex flex-column   pt-3">
-          <v-col class="align-center text-end">
-            <v-btn v-for="btn in copyBtns" class="text-body-1" @click="btn.copyFn" :key="btn.id" small variant="text"
+        <v-row class="d-flex pt-3 justify-end">
+          <v-col cols="6" class="flex-column align-center text-end">
+            <v-btn v-for="btn in copyBtns" class="text-body-2" @click="btn.copyFn" :key="btn.id" small variant="text"
               :prepend-icon="[mdiContentCopy]">
               copy as {{ btn.copyAs }}
             </v-btn>
@@ -37,7 +37,7 @@
 
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="primary" variant="text" @click="dialog = !dialog">
+        <v-btn variant="text" @click="dialog = !dialog">
           Close
         </v-btn>
       </v-card-actions>
@@ -52,7 +52,7 @@ import { computed, ref } from "vue";
 import { getLayers } from "@/store/Actions";
 import { mapPosition } from "@/store/States";
 
-const dialog = defineModel({ type: Boolean });
+const dialog = defineModel({ type: Boolean,required:true,default:false });
 
 const props = defineProps({
   for: {
@@ -88,22 +88,19 @@ const mapStepCode = computed(() => {
 #### Tour step title
 Text describing the current step of the tour and why it is interesting what the map shows currently
 `;
-  return `${preTag}${JSON.stringify(getLayers(props?.for))}' ${endTag}`
+  return `${preTag}'${JSON.stringify(getLayers(props?.for))}' ${endTag}`
 })
 const mapEntryCode = computed(() => {
   const [x, y, z] = mapPosition.value
   const preTag = '## Map Example <!--{as="eox-map" style="width: 100%; height: 500px;" layers=';
   const endTag = `zoom="${z}" center=[${[x, y]}] }-->`;
-  return `${preTag}${JSON.stringify(getLayers(props?.for))}' ${endTag}`;
+  return `${preTag}'${JSON.stringify(getLayers(props?.for))}' ${endTag}`;
 })
 </script>
 <style scoped>
-.background-primary {
-  background: rgb(var(--v-theme-primary));
-}
-
 .code-block {
   background-color: #ddd;
   font-family: monospace;
+  font-size: small;
 }
 </style>
