8a0610160c8284168b9be4cbfc8e2aa295847029
A-Behairi <behairiahmed@gmail.com>
2024-07-09 18:53:25 +0200

    fix: get layers in lib mode

--- core/client/store/Actions.js
+++ core/client/store/Actions.js
@@ -4,5 +4,8 @@
  * @returns {object[]}
  */
 export const getLayers = (el = "eox-map") =>
-  //@ts-expect-error layers doesn't exist on type element
-  document.querySelector(el)?.layers.toReversed();
+  customElements.get('eo-dash') ?
+    //@ts-expect-error layers doesn't exist on type element
+    document.querySelector('eo-dash')?.shadowRoot?.querySelector(el)?.layers.toReversed() :
+    //@ts-expect-error layers doesn't exist on type element
+    document.querySelector(el)?.layers.toReversed()
