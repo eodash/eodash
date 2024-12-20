823551e1575ca554b049112397f173aa0aad01db
A-Behairi <behairiahmed@gmail.com>
2024-12-20 16:08:44 +0200

    test: increase timeout

--- tests/cypress/components/DashboardLayout.cy.js
+++ tests/cypress/components/DashboardLayout.cy.js
@@ -34,7 +34,7 @@ describe("<DashboardLayout />", () => {
 
   it("mount successfully", () => {
     cy.get("@vue").then(() => {
-      cy.get("main", { timeout: 10000 }).should("exist");
+      cy.get("main", { timeout: 15000 }).should("exist");
     });
   });
 
