70049e53ac5563db6fb177c91d45d64390c48f5d
Daniel Santillan <daniel.santillan@eox.at>
2025-09-18 17:31:22 +0200

    chore: fix typing

--- widgets/EodashMap/methods/index.js
+++ widgets/EodashMap/methods/index.js
@@ -120,7 +120,7 @@ export const useInitMap = (
           layersCollection = await createLayersConfig(
             updatedStac,
             eodashCols,
-            [...mapElement.value?.layers].reverse(),
+            [...(mapElement.value?.layers ?? [])].reverse(),
             updatedTime,
           );
           log.debug(
@@ -143,7 +143,7 @@ export const useInitMap = (
         layersCollection = await createLayersConfig(
           updatedStac,
           eodashCols,
-          [...mapElement.value?.layers].reverse(),
+          [...(mapElement.value?.layers ?? [])].reverse(),
           datetime.value,
         );
 
