4f1440b5f056c8afc17a010a3ee88ae964a9d7c5
Lubomir <lubomir.dolezal@eox.at>
2025-10-28 14:17:51 +0100

    fix: typo

--- core/client/eodashSTAC/createLayers.js
+++ core/client/eodashSTAC/createLayers.js
@@ -141,10 +141,7 @@ export async function createLayersFromAssets(
       source: {
         type: "Vector",
         url: await mergeGeojsons(geoJsonSources),
-        format: {
-          "type": "GeoJSON",
-          "dataProjection": dataProjection
-        },
+        format: "GeoJSON",
         attributions: geoJsonAttributions,
       },
       properties: {
