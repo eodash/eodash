6c0d229915f129e677a36a25b5e78a861ca6893b
Lubomir Dolezal <lubojr@seznam.cz>
2026-06-08 11:42:53 +0200

    chore: lint

--- templates/baseConfig.js
+++ templates/baseConfig.js
@@ -21,7 +21,8 @@ const baseConfig = {
       { url: "api.explorer.eopf.copernicus.eu", titilerVersion: 2 },
     ],
     rasterEndpoint: "https://api.explorer.eopf.copernicus.eu/rstaging",
-    colormapRegistry: "https://raw.githubusercontent.com/eurodatacube/eodash-assets/refs/heads/rasterform/defaults/colormaps.json"
+    colormapRegistry:
+      "https://raw.githubusercontent.com/eurodatacube/eodash-assets/refs/heads/rasterform/defaults/colormaps.json",
   },
   brand: {
     noLayout: true,
