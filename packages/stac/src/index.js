bab08e6dc8e3694bc9864bd56674710e07ae5dd3
A-Behairi <behairiahmed@gmail.com>
2026-08-19 16:40:54 +0200

    feat(stac): define package and STAC types

--- packages/stac/src/index.js
+++ packages/stac/src/index.js
@@ -1,1 +1,31 @@
-export {};
+import axios from "axios";
+import { findParquetMirror } from "./helpers/assets.js";
+import { createAPICollection } from "./collections/api.js";
+import { createParquetCollection } from "./collections/parquet.js";
+import { createStaticCollection } from "./collections/static.js";
+
+/**
+ * Reads a collection and returns the reader that resolves its items.
+ *
+ * @param {string} url collection url
+ * @param {object} [options]
+ * @param {boolean} [options.api] the catalog is served by a STAC API, which the collection document cannot state
+ * @param {number} [options.maxItems] most items one api search returns
+ * @returns {Promise<import("./types").CollectionReader>}
+ */
+export const createEodashCollection = async (url, options = {}) => {
+  const { api = false, maxItems } = options;
+  /** @type {import("./types").EodashCollection} */
+  const stac = await axios.get(url).then((response) => response.data);
+  const context = { url, stac };
+
+  if (api) {
+    return createAPICollection({ ...context, maxItems });
+  }
+
+  if (findParquetMirror(stac)) {
+    return createParquetCollection(context);
+  }
+
+  return createStaticCollection(context);
+};
